7d2b8c71f970d07f87a0c8a6797e4c93c7581dd7
Sahil <issahil404@gmail.com>
2025-10-05 01:13:21 +0530

    added password reset functionality

--- src/schemas/mod.rs
+++ src/schemas/mod.rs
@@ -1,5 +1,7 @@
 pub mod auth_schemas;
+pub mod password_reset_schemas;
 pub mod user_schemas;
 
 pub use auth_schemas::*;
+pub use password_reset_schemas::*;
 pub use user_schemas::{CreateUserRequest, UpdateUserRequest, UserResponse};
